930bf99990736d4aad8efe216c5a50978059d003
This Pc <ankit.singh_cs18@gla.ac.in>
2020-03-24 22:58:34 +0530

    Problem 3 solution

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,20 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem5.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue obj=new MyPriorityQueue();
+        obj.enqueue((new Student(181500100)).getRollNum());
+        obj.enqueue((new Student(181500533)).getRollNum());
+        obj.enqueue((new Student(181500233)).getRollNum());
+        obj.enqueue((new Student(181500002)).getRollNum());
+        obj.enqueue((new Student(181500456)).getRollNum());
+        obj.traverse();
+    }
 }
--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,5 +6,48 @@
  */
 package problem3.myqueue;
 
+import problem3.node.Node;
+
 public class MyPriorityQueue {
+    Node front;
+    Node rear;
+    int size;
+
+
+    public void enqueue(int data) {
+        Node node = new Node(data);
+        if (front==null) {
+            front = rear = node;
+        } else if (data < front.getData()) {
+            Node temp = front;
+            node.setNext(temp);
+            front = node;
+        } else {
+            Node current = front;
+            while (current.getNext() != null && current.getNext().getData() < data) {
+                current = current.getNext();
+            }
+            Node temp = current.getNext();
+            node.setNext(temp);
+            current.setNext(node);
+        }
+        this.size++;
+    }
+
+
+
+    public void traverse(){
+        Node temp=front;
+        if(front==null){
+            System.out.println("queue is empty");
+        }
+        else{
+            while (temp.getNext() != null){
+                System.out.print(temp.getData()+" ");
+                temp=temp.getNext();
+            }
+            System.out.println(temp.getData());
+        }
+    }
+
 }
