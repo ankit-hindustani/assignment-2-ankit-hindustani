307826edcfd8436eb67804132b0370d156ebe2e6
This Pc <ankit.singh_cs18@gla.ac.in>
2020-03-26 01:37:11 +0530

    Problem 2 solution

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -1,11 +1,39 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:15 PM
+ *  Author: Ankit Singh
+ *  GLA university mathura (u.p)
  */
 package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
 // executable class
-// use problem1.mybst.MyBinarySearchTree class for creating binary tree
+// used problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj=new MyBinarySearchTree();
+        obj.insert(115);
+        obj.insert(32);
+        obj.insert(42);
+        obj.insert(9);
+        obj.insert(244);
+
+        System.out.println("tree root= "+obj.getRoot().getData());
+        obj.traversePreOrder(obj.getRoot());
+
+        //after execution of the program, we see that in preOrder traverse, root is first element
+
+
+        System.out.println();
+        obj.traversePostOrder(obj.getRoot());
+
+        //after execution of the program, we see that in postOrder traverse, root is last element
+
+
+
+        /* Problem 2 (b)
+        but in both traverse(pre,post) mid point is different.
+         */
+
+    }
 }
