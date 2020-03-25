3a9c80aaede0bf64751b0504d2e42804e5590d0f
This Pc <ankit.singh_cs18@gla.ac.in>
2020-03-26 01:43:40 +0530

    Problem 2 related code done

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -1,10 +1,64 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
+ *  Author: Ankit Singh
+ *  GLA university mathura (u.p)
  */
 package problem1.mybst;
 // to implement BinarySearchTree
+import problem1.node.TreeNode;
+
 public class MyBinarySearchTree {
-}
+
+        TreeNode root;
+
+        public TreeNode getRoot(){
+            return root;
+        }
+        //insert
+        public void insert(int data){
+            TreeNode node=new TreeNode(data);
+            if(root==null){
+                root=node;
+            }
+            else{
+                TreeNode temp=root;
+                TreeNode parent=null;
+                while(temp!=null){
+                    parent=temp;
+                    if(node.getData()<=temp.getData()){
+                        temp=temp.getLeft();
+                    }
+                    else{
+                        temp=temp.getRight();
+                    }
+                }
+                if(node.getData()<=parent.getData()){
+                    parent.setLeft(node);
+                }
+                else{
+                    parent.setRight(node);
+                }
+            }
+
+        }
+
+    public void traversePreOrder(TreeNode node){
+        if(node != null){
+            System.out.print(node.getData()+" ");
+            traversePreOrder(node.getLeft());
+            traversePreOrder(node.getRight());
+
+        }
+    }
+
+    public void traversePostOrder(TreeNode node){
+        if(node != null){
+            traversePostOrder(node.getLeft());
+            traversePostOrder(node.getRight());
+            System.out.print(node.getData()+" ");
+        }
+    }
+
+    }
+
+
--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -1,10 +1,40 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
+ *  Author: Ankit Singh
+ *  GLA university mathura (u.p)
  */
 package problem1.node;
 
 public class TreeNode {
+    private int data;
+    private TreeNode left;
+    private TreeNode right;
+
+    public TreeNode(int data){
+        this.data=data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
 }
