76ac53dd2f7266c86e7603211de4ca77c0ebecbd
This Pc <ankit.singh_cs18@gla.ac.in>
2020-03-29 02:23:20 +0530

    problem 4 complete without all test.

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,7 +5,33 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        //create a BST and enter some element
+        MyBinarySearchTree BST=new MyBinarySearchTree();
+        BST.insert(10);
+        BST.insert(5);
+        BST.insert(8);
+        BST.insert(6);
+        BST.insert(20);
+        BST.insert(15);
+        BST.insert(9);
+        BST.insert(11);
+        BST.insert(12);
+        BST.insert(13);
+        BST.insert(4);
+//create a queue and passing BST for find pre-order succesor
+        MyQueue obj=new MyQueue();
+        obj.enqueue(BST);
+//print Queue
+        obj.printQueue();
+
+
+    }
+
 }
--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -1,11 +1,68 @@
 /*
+ *
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:11 PM
+ *  Author: Ankit Singh
+ *  GLA university mathura (U.P)
+ *
  */
-package problem4.myqueue;
-// to create queue to store pre - order successor
 
+package problem4.myqueue;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+import problem3.node.Node;
+
+import java.util.ArrayList;
+
+// to create queue to store pre - order successor
 public class MyQueue {
-}
+    Node front;  //head
+    Node rear;
+    static ArrayList ls=new ArrayList();
+
+    //Finding Pre-order and store it into ArrayList
+    public void traversePreOrder(TreeNode node) {
+        if (node != null) {
+            ls.add(node.getData());
+            traversePreOrder(node.getLeft());
+            traversePreOrder(node.getRight());
+
+        }
+    }
+//insert pre-order succesor of all nodes into queue
+    public void enqueue(MyBinarySearchTree BST){
+        TreeNode root=BST.getRoot();
+        //calling for pre-order
+        traversePreOrder(root);
+        //enter all pre-order element into queue except first element
+        for(int i=1;i<ls.size();i++){
+            int element= (int) ls.get(i);
+            Node node=new Node(element);
+            if(front==null){
+                front=rear=node;
+
+            }
+            else{
+                rear.setNext(node);
+                rear=rear.getNext();
+            }
+        }
+    }
+
+    //printing queue
+    public void printQueue(){
+            Node temp=front;
+            if(front==null){
+                System.out.println("queue is empty");
+            }
+            else{
+                while (temp.getNext() != null){
+                    System.out.print(temp.getData()+" ");
+                    temp=temp.getNext();
+                }
+                System.out.println(temp.getData());
+            }
+        }
+
+    }
+
